88fbe5cb27c0e01390f2618e870ef359ca4ebc6d
Yan Zhu <picturepan2@hotmail.com>
2017-11-02 21:36:06 +0800

    Add archives to footer

--- footer.php
+++ footer.php
@@ -13,6 +13,11 @@
         <span class="divider-dot">·</span>
         <a href="/links">友链</a>
       </nav>
+      <nav class="footer-nav">
+        <select class="form-select select-sm select-inline" name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+          <?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?>
+        </select>
+      </nav>
       <section class="copyright">&copy; 2007 - 2017 <?php bloginfo( 'name' ); ?> 保留一切权利</section>
       <section class="copyright">Proudly published with <a href="https://wordpress.org/" target="_blank">WordPress</a>. Theme <a href="https://github.com/picturepan2/thea" target="_blank">Thea</a> made with <span class="text-error">&hearts;</span>. </section>
       <?php if ( is_user_logged_in() ) {?><section class="copyright"><?php echo get_num_queries(); ?> Queries, <?php timer_stop(1); ?> Seconds</section><?php } ?>
