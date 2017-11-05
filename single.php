09a36b16cdee12d8610db6a3c8c44eebdbd7f0b0
Yan Zhu <picturepan2@hotmail.com>
2017-11-05 12:05:17 +0800

    Add related posts

--- single.php
+++ single.php
@@ -58,9 +58,24 @@
         </div>
       </div>
 
+      <div class="post-related">
+        <div class="ads post-ads right pr">
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-format="autorelaxed"
+              data-ad-client="ca-pub-2225124559530218"
+              data-ad-slot="5206161521"></ins>
+          <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        </div>
+      </div>
+
       <div class="post-comments">
         <?php comments_template(); ?>
-        </div>
+      </div>
+
     </article>
 
     <?php endwhile; else: ?>
